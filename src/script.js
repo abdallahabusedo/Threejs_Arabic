26c00423a2c6d16fa5c9a6de58243d4d66212954
abdallahabusedo <abdallahsedo@hotmail.com>
2023-05-05 19:11:04 +0300

    textures and materials

--- src/script.js
+++ src/script.js
@@ -1,36 +1,30 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import gsap from "gsap"; // npm install gsap
-
-/**
- * ! Step 1
- * npm install --save lil-gui
- */
-
-/**
- * ! Step 2
- */
 import * as dat from "lil-gui";
+
 /**
  * Debug
  */
 const gui = new dat.GUI();
-const parameters = {
-  color: 0xff0000,
-  spin: () => {
-    gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 });
-  },
-};
+const parameters = {};
 
+/**
+ * Scene
+ */
 const scene = new THREE.Scene();
-
 const canvas = document.querySelector("canvas.webgl");
 
+/**
+ * Sizes Object
+ */
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
 
+/**
+ * Resize Event Listener
+ */
 window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
@@ -41,51 +35,71 @@ window.addEventListener("resize", () => {
 });
 
 /**
- * Cube
+ * Meshes
  */
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(4, 4, 4),
-  new THREE.MeshBasicMaterial({ color: parameters.color })
-);
-scene.add(mesh);
+//! Cube
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cubeGeometry = new THREE.BoxGeometry(4, 4, 4, 512, 512, 512);
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
 
+//! Sphere
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const sphereGeometry = new THREE.SphereGeometry(2, 512, 512);
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.x = 10;
+scene.add(sphere);
+
+//! turos
+const toursMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const toursGeometry = new THREE.TorusGeometry(2, 0.4, 512, 512);
+const tours = new THREE.Mesh(toursGeometry, toursMaterial);
+tours.position.x = -10;
+scene.add(tours);
 /**
- * Tweaks
+ * Camera
  */
-gui.add(mesh.position, "y");
-gui.add(mesh.position, "x", -3, 3, 0.01);
-gui.add(mesh.position, "z").min(-3).max(3).step(0.01).name("elevation");
-gui.add(mesh, "visible");
-gui.add(mesh.material, "wireframe");
-console.log(mesh.material.color);
-gui.addColor(parameters, "color").onChange(() => {
-  mesh.material.color.set(parameters.color);
-});
-gui.add(parameters, "spin");
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
   100
 );
-camera.position.z = 20;
+camera.position.z = 15;
 scene.add(camera);
 
+/**
+ * Controls
+ */
 const controls = new OrbitControls(camera, canvas);
 
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
-
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
+/**
+ * Clock
+ */
 const clock = new THREE.Clock();
 
-const tick = () => {
+/**
+ * Animate Function
+ */
+const animate = () => {
   const elapsedTime = clock.getElapsedTime();
 
+  cube.rotation.y = 0.1 * elapsedTime;
+  sphere.rotation.y = 0.1 * elapsedTime;
+  tours.rotation.y = 0.1 * elapsedTime;
+  cube.rotation.x = 0.15 * elapsedTime;
+  sphere.rotation.x = 0.15 * elapsedTime;
+  tours.rotation.x = 0.15 * elapsedTime;
   renderer.render(scene, camera);
-  window.requestAnimationFrame(tick);
+  window.requestAnimationFrame(animate);
 };
-tick();
+animate();
