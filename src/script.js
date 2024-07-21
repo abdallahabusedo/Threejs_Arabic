fd40f7f9c3fcd3f0c7d0c6404d90c5a02e9e3f10
abdallah abu sedo <42722816+abdallahabusedo@users.noreply.github.com>
2024-07-21 12:35:10 +0300

    chore: Update script.js with GLTFLoader and add 3D model to scene

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
 const scene = new THREE.Scene();
 
 const canvas = document.querySelector("canvas.webgl");
@@ -18,19 +20,43 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(4, 4, 4),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-scene.add(cube);
+let gltfMesh;
+let particles;
+const loader = new GLTFLoader();
+loader.load("/train/scene.gltf", (gltf) => {
+  gltfMesh = gltf.scene;
+  // turn gltf to points
+
+  gltfMesh.traverse((child) => {
+    if (child instanceof THREE.Mesh) {
+      child.material = new THREE.PointsMaterial({
+        size: 0.02,
+        sizeAttenuation: true,
+      });
+    }
+  });
+
+  scene.add(gltfMesh);
+});
+
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
-  100
+  10000
 );
-camera.position.z = 20;
+camera.position.z = 10;
 scene.add(camera);
+
+// lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
 
 const controls = new OrbitControls(camera, canvas);
 
@@ -42,9 +68,22 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 const clock = new THREE.Clock();
-
+let oldElapsedTime = 0;
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
+  // animate particles random
+  if (particles) {
+    const positions = particles.geometry.attributes.position.array;
+    for (let i = 0; i < positions.length; i += 3) {
+      positions[i + 1] -= 0.1 * deltaTime;
+      if (positions[i + 1] < -10) {
+        positions[i + 1] = 10;
+      }
+    }
+    particles.geometry.attributes.position.needsUpdate = true;
+  }
 
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
